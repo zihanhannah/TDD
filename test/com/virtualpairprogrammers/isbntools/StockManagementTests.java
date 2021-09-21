9ab33c17be29a3d0096dd961f695e16b9cc59e9b
zihan <zihanhannah@gmail.com>
2021-09-20 20:59:13 -0500

    create the stub

--- test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
+++ test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
@@ -1,4 +1,25 @@
 package com.virtualpairprogrammers.isbntools;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class StockManagementTests {
+    @Test
+    public void testCanGetACorrectLocatorCode(){
+//        fail();
+        ExternalISBNDataService testService = new ExternalISBNDataService() {
+            @Override
+            public Book lookup(String isbn) {
+                return new Book(isbn, "Of Mice And Men", "J. Steinbeck");
+            }
+        };
+        StockManager stockManager = new StockManager();
+        stockManager.setService(testService);
+
+        String isbn = "0140177396";
+        String locatorCode = stockManager.getLocatorCode(isbn);
+        assertEquals("7396J4",locatorCode);
+    }
 }
